2445b4d07d91e043398e7afd4cac6e4712ee1e64
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-01-21 16:32:35 -0300

    #008 adding characters grid

--- frontend/src/components/CharacterList.jsx
+++ frontend/src/components/CharacterList.jsx
@@ -1,41 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'; // Para navegação entre páginas, se for o caso
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom"; // Para navegação entre páginas, se for o caso
 
 const CharacterList = () => {
-    const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-    useEffect(() => {
-        const fetchCharacters = async () => {
-            const response = await fetch('http://localhost:3001/api/character');
-            const data = await response.json();
-            console.log(data);
-            setCharacters(data.results);
-        };
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const response = await fetch("http://localhost:3001/api/character");
+      const data = await response.json();
+      console.log(data);
+      setCharacters(data.results);
+    };
 
-        fetchCharacters();
-    }, []);
+    fetchCharacters();
+  }, []);
 
-    if (!Array.isArray(characters)) {
-        console.error('characters não é um array', characters);
-        return <div>Erro ao carregar personagens</div>;
-    }
+  if (!Array.isArray(characters)) {
+    console.error("characters não é um array", characters);
+    return <div>Erro ao carregar personagens</div>;
+  }
 
-    if (characters.length === 0) return <div>Loading...</div>;
+  if (characters.length === 0) return <div>Loading...</div>;
 
-    return (
-      <section className="w-full p-8">
-        <div className='container'>
-
-          <div className="p-8 m-8 items-center">
-            <h1 className="section-title p-2">Rick and Morty Wiki</h1>
-          </div>
-
-
-          
-
+  return (
+    <section className="w-full">
+      <div className="p-12 w-full">
+        <div className="p-8 m-8 items-center">
+          <h1 className="section-title p-2">Rick and Morty Wiki</h1>
         </div>
-      </section>
-    );
+
+        <div className="w-full h-auto p-4 items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+                {characters.map((character) => (
+                    <div
+                    key={character.id}
+                    className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
+                    >
+                    <Link to={`/character/${character.id}`}>
+                        <img
+                        src={character.image}
+                        alt={character.name}
+                        className="w-full h-56 object-cover"
+                        />
+                        <div className="p-1">
+                        <h2 className="text-lg font-bold text-center">
+                            {character.name}
+                        </h2>
+                        </div>
+                    </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+        
+
+      </div>
+    </section>
+  );
 };
 
 export default CharacterList;
