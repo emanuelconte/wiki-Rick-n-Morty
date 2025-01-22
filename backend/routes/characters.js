4a8c7e6ee8e487c7501bfd44314ee1016f6f70e0
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-01-21 21:19:29 -0300

    #011 fixing backend for multiple pages

--- backend/routes/characters.js
+++ backend/routes/characters.js
@@ -7,10 +7,13 @@ const API_BASE_URL = 'https://rickandmortyapi.com/api';
 // Rota para listar personagens
 router.get('/character', async (req, res) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/character`);
+        const page = req.query.page || 1;
+        const response = await axios.get(`${API_BASE_URL}/character?page=${page}`);
+        
         res.json(response.data);
     } catch (error) {
-        res.status(500).json({ error: 'Erro ao buscar personagens.' });
+        console.error('Error fetching characters:', error.message);
+        res.status(500).json({ error: 'Error fetching characters.' });
     }
 });
 
@@ -21,12 +24,12 @@ router.get('/character/:id', async (req, res) => {
     try {
         const character = characters.find(c => c.id === parseInt(id));
         if (!character) {
-            return res.status(404).json({ error: 'Personagem não encontrado' });
+            return res.status(404).json({ error: 'Character not found' });
         }
         console.log(character)
         res.json(character);
     } catch (error) {
-        res.status(500).json({ error: 'Erro ao buscar o personagem.' });
+        res.status(500).json({ error: 'Error fetching characters.' });
     }
 });
 
