d7f7d9361b373f0a04201d49049138c091ebf752
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-01-23 14:32:44 -0300

    #023 adding styles to characters details

--- frontend/src/components/CharacterDetails.jsx
+++ frontend/src/components/CharacterDetails.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 const CharacterDetails = () => {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
+  const [episodes, setEpisodes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const goToHome = () => {
@@ -18,6 +19,12 @@ const CharacterDetails = () => {
         );
         const data = await response.json();
         setCharacter(data);
+
+        const episodePromises = data.episode.map((episodeUrl) =>
+          fetch(episodeUrl).then((res) => res.json())
+        );
+        const episodeData = await Promise.all(episodePromises);
+        setEpisodes(episodeData);
       } catch (error) {
         console.error("Error fetching character:", error);
       } finally {
@@ -33,28 +40,49 @@ const CharacterDetails = () => {
 
   return (
     <section className="w-full">
-      <div className="p-8 m-8 items-center add-border">
-        <h1 
-          className="section-title p-2" 
+      <div className="p-8 m-8 items-center">
+        <h1
+          className="section-title p-2"
           onClick={goToHome}
-          style={{ cursor: "pointer" }} >
-            Rick and Morty Wiki
+          style={{ cursor: "pointer" }}
+        >
+          Rick and Morty Wiki
         </h1>
       </div>
 
-      <div className="m-12 p-8 flex flex-row">
-        <div className="flex flex-col">
-            <h1 className="char-name p-4 w-[368px] items-center">{character.name}</h1>
-            <img className="w-[368px] h-[368px] rounded-xl" src={character.image} alt={character.name} />
-        </div>
+      <div className="m-12 p-8 flex flex-col">
+          <div className="flex flex-row">
+            <div className="flex flex-col">
+              <h1 className="char-name p-4 w-[368px]">
+                {character.name}
+              </h1>
+              <img
+                className="w-[368px] h-[368px] rounded-xl"
+                src={character.image}
+                alt={character.name}
+              />
+            </div>
+            <div className="mt-auto ml-4">
+              <p className="status-char">Status: {character.status}</p>
+              <p className="status-char">Species: {character.species}</p>
+              <p className="status-char">Gender: {character.gender}</p>
+              <p className="status-char">Origin: {character.origin.name}</p>
+            </div>
+          </div>
 
-        <div>
-            <p>Status: {character.status}</p>
-            <p>Species: {character.species}</p>
-            <p>Gender: {character.gender}</p>
-            <p>Origin: {character.origin.name}</p>
-        </div>
+          <div className="">
+            <h2 className="text-2xl mt-6 char-name">Episodes:</h2>
+            <ul className="list-disc list-inside text-white">
+              {episodes.map((episode) => (
+                <li key={episode.id}>
+                  <strong>{episode.name}</strong> - Air Date: {episode.air_date}
+                </li>
+              ))}
+            </ul>
+          </div>
         
+        
+
       </div>
     </section>
   );
