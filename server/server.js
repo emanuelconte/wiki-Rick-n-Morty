acc776868fb0ca0491c1853aff43b3d04e92685f
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-01-24 13:38:06 -0300

    #029 fixing backend server

--- server/server.js
+++ server/server.js
@@ -10,10 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Rick and Morty API!');
+});
+
 app.use('/api', characterRoutes);
 
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
