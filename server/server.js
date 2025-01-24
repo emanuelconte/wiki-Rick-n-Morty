5bef0dd437cfea294bb157f20a56219aaff0395d
Emanuel Conte <emanuelcontecardoso@gmail.com>
2025-01-24 14:03:56 -0300

    #031 fixing vercel.json on server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const characterRoutes = require('./routes/characters');
 
-
-dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/api', characterRoutes);
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Rick and Morty API!');
 });
-
-app.use('/api', characterRoutes);
 
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
